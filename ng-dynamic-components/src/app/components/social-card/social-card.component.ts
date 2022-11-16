7dc97ad3185206b8afe9a7933813ee2959bc8416
ElideZavala <zavalavinagreelide@gmail.com>
2022-11-16 11:50:34 -0600

    creacion de un componente dinamico

--- ng-dynamic-components/src/app/components/social-card/social-card.component.ts
+++ ng-dynamic-components/src/app/components/social-card/social-card.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ComponentFactoryResolver, ViewChild, ViewContainerRef, SimpleChanges } from '@angular/core';
 import { SocialCardType } from 'src/app/constants/social-card-type';
+import { FbCardComponent } from '../fb-card/fb-card.component';
+import { TwitterCardComponent } from '../twitter-card/twitter-card.component';
 
 @Component({
   selector: 'app-social-card',
@@ -7,9 +9,48 @@ import { SocialCardType } from 'src/app/constants/social-card-type';
   styleUrls: ['./social-card.component.scss']
 })
 export class SocialCardComponent implements OnInit {
+  // sera la varivale que se le asigne al componente
   @Input() type: SocialCardType;
+
+  // referencia al contenedor donde se cargara el componente dinamico
+  @ViewChild('vrf', { read: ViewContainerRef }) vrf: ViewContainerRef;
+
+  // metodo que se ejecuta cuando el componente es destruido
   cardTypes = SocialCardType;
-  constructor() { }
+
+  constructor(
+    private componentFactoryResolver: ComponentFactoryResolver
+  ) { }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.type.currentValue !== undefined) {
+      // console.log(`card type changed to: ${changes.type.currentValue}`);
+      this.loadDinamicComponent(changes.type.currentValue);
+    }
+  }
+
+  loadDinamicComponent(type: SocialCardType): void {
+    let component;
+    switch (type) {
+      case SocialCardType.Facebook:
+        component = FbCardComponent;
+        break;
+      case SocialCardType.Twitter:
+        component = TwitterCardComponent;
+        break;
+    }
+
+
+    // se crea el componente dinamico
+    const componentFactory = this.componentFactoryResolver.resolveComponentFactory(component)
+
+    // se limpia el contenedor antes de crear el nuevo componente
+    this.vrf.clear();
+
+    // elemento donde se cargara el componente
+    this.vrf.createComponent(componentFactory);
+
+  }
 
   ngOnInit(): void {
   }
